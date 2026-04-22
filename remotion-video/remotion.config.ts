76f16aa969f6c9b8f486bae2936daae5167031f9
RajaChaiban <rajachaiban@gmail.com>
2026-04-21 23:38:18 -0400

    chore: initialize Remotion project with TypeScript config

---
+++ remotion-video/remotion.config.ts
@@ -0,0 +1,8 @@
+import { Config } from 'remotion';
+
+Config.setVideoImageFormat('png');
+Config.setCodec('h264');
+Config.setAudioBitrate('192k');
+Config.setFps(30);
+Config.setCrf(18);
+Config.setPixelFormat('yuv420p');
